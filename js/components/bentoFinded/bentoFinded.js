dddcf9225e6aa63ad002eb302b72ecd4a61f78c0
DaroOps <romerosd089@hotmail.com>
2024-05-13 03:42:34 -0500

    feat: :sparkles: Add working observer and implemented logic

--- js/components/bentoFinded/bentoFinded.js
+++ js/components/bentoFinded/bentoFinded.js
@@ -1,53 +1,45 @@
+import { eventBus } from "../../global.js";
+
 export class BentoFinded extends HTMLElement{
-    id
-    constructor(id){
+    constructor(){
         super();
         this.attachShadow({mode: "open"});
+        eventBus.subscribe('dataReceived', this.connectedCallback.bind(this));
     }
-    connectedCallback(){
-        
-        this.shadowRoot.innerHTML = /*html*/`
-        <link rel="stylesheet" href="../css/bentoFinded.css">
-        <div class="bento-finded">
-            <section class="bento-grid">
+    connectedCallback(data = null){
+        if (data){
+            const albums = data.albums.items
+            const fragment  = document.createDocumentFragment();
+            
+                        this.shadowRoot.innerHTML = /*html*/`
+                        <link rel="stylesheet" href="../css/bentoFinded.css">
+                        <div class="bento-finded">
+                            <section class="bento-grid"></section>
+                        </div>
+                    `;
 
-                <div class="item-playable-container">
-                    <img src="https://i.scdn.co/image/ab67616d00001e02fa258529452f4ed34cc961b1" alt="img">
-                    <p>Item Name</p>
-                    <small>item-category</small>
-                </div>
-
-                <div class="item-playable-container">
-                    <img src="https://i.scdn.co/image/ab67616d00001e02fa258529452f4ed34cc961b1" alt="img">
-                    <p>Item Name</p>
-                    <small>item-category</small>
-                </div>
-
-                <div class="item-playable-container">
-                    <img src="https://i.scdn.co/image/ab67616d00001e02fa258529452f4ed34cc961b1" alt="img">
-                    <p>Item Name</p>
-                    <small>item-category</small>
-                </div>
-
-                <div class="item-playable-container">
-                    <img src="https://i.scdn.co/image/ab67616d00001e02fa258529452f4ed34cc961b1" alt="img">
-                    <p>Item Name</p>
-                    <small>item-category</small>
-                </div>
-
-                
-            </section>
-        </div>
-       `
+            albums.forEach(album => {
+                const albumCard = document.createElement('album-card');
+                albumCard.setAttribute('item-name', album.data.name)
+                albumCard.setAttribute('item-category', album.data.artists?.items[0]?.profile.name)
+                albumCard.setAttribute('img-url', album.data.coverArt?.sources[0]?.url)
+                fragment.appendChild(albumCard)
+            });
+            
+            this.shadowRoot.querySelector('.bento-grid').appendChild(fragment)
+        }
+        else{
+            this.shadowRoot.innerHTML = /*html*/`
+            <link rel="stylesheet" href="../css/bentoFinded.css">
+            <div class="bento-finded">
+                <section class="bento-grid">
+                    <album-card></album-card>
+                    <album-card></album-card>
+                    <album-card></album-card>
+                    <album-card></album-card>
+                </section>
+            </div>
+        `
+        }      
     }
-    // static get observedAttributes(){
-    //     return ["uri"];
-    // }
-    // attributeChangedCallback(name,old,now){
-    //     let[nameUri, album, id] = now.split(":")
-    //     this.id = id;
-    // }
 }
-
-
-
