494fc178a4acf4288c07c981db1b288c9b2c5442
DaroOps <romerosd089@hotmail.com>
2024-05-12 23:18:21 -0500

    feat: :construction: Add import helper

---
+++ js/components/components.js
@@ -0,0 +1,6 @@
+import {PlaylistTracker} from "./playlistTracker/playlistTracker.js"
+import {SearchBar} from "./searchbar/searchbar.js"
+import { BentoFinded } from "./bentoFinded/bentoFinded.js"
+import { TrackCard } from "./trackCard/trackCard.js"
+
+export {PlaylistTracker, SearchBar, BentoFinded, TrackCard}
--- js/index.js
+++ js/index.js
@@ -1,4 +1,6 @@
-import { PlaylistTracker } from "./components/playlistTracker/playlistTracker.js";
-
+import { BentoFinded, PlaylistTracker, SearchBar, TrackCard} from "./components/components.js";
 
 customElements.define("playlist-tracker", PlaylistTracker);
+customElements.define("search-bar", SearchBar);
+customElements.define("bento-finded", BentoFinded)
+customElements.define("track-card", TrackCard)
