cf3d625d4e90c8053b0371791df3a030e21e8d3d
DaroOps <romerosd089@hotmail.com>
2024-05-14 02:10:39 -0500

    feat: :construction: Add simulated click when the page is loaded

--- js/components/bentoFinded/bentoFinded.js
+++ js/components/bentoFinded/bentoFinded.js
@@ -1,62 +1,73 @@
 import { eventBus } from "../../global.js";
 
-export class BentoFinded extends HTMLElement{
-    constructor(){
+export class BentoFinded extends HTMLElement {
+    constructor() {
         super();
-        this.attachShadow({mode: "open"});
-        eventBus.subscribe('dataReceived', this.connectedCallback.bind(this));
+        this.attachShadow({ mode: "open" });
+        eventBus.subscribe('dataReceived', this.render.bind(this));
+        window.addEventListener('load', this.onPageLoaded.bind(this));
+        firstAlbumCard
     }
-    connectedCallback(data = null){
-        if (data){
-            const albums = data.albums.items
-            const fragment  = document.createDocumentFragment();
-            
-                        this.shadowRoot.innerHTML = /*html*/`
-                        <link rel="stylesheet" href="../css/bentoFinded.css">
-                        <div class="bento-finded">
-                            <section class="bento-grid"></section>
-                        </div>
-                    `;
+
+    connectedCallback() {
+        this.render();
+    }
+
+    render(data = null) {
+        if (data) {
+            const albums = data.albums.items;
+            const fragment = document.createDocumentFragment();
+
+            this.shadowRoot.innerHTML = /*html*/`
+                <link rel="stylesheet" href="../css/bentoFinded.css">
+                <div class="bento-finded">
+                    <section class="bento-grid"></section>
+                </div>
+            `;
 
             albums.forEach(album => {
                 const albumCard = document.createElement('album-card');
-                albumCard.setAttribute('item-name', album.data.name)
-                albumCard.setAttribute('item-category', album.data.artists?.items[0]?.profile.name)
-                albumCard.setAttribute('img-url', album.data.coverArt?.sources[0]?.url)
-                let [origin, endpoint, id] = album.data.uri.split(":")
-                albumCard.id = id
+                albumCard.setAttribute('item-name', album.data.name);
+                albumCard.setAttribute('item-category', album.data.artists?.items[0]?.profile.name);
+                albumCard.setAttribute('img-url', album.data.coverArt?.sources[0]?.url);
+                let [origin, endpoint, id] = album.data.uri.split(":");
+                albumCard.id = id;
 
                 albumCard.addEventListener('click', () => {
                     this.handleAlbumClick(id);
                 });
-                
 
-                fragment.appendChild(albumCard)
+                fragment.appendChild(albumCard);
             });
-            
-            
-            this.shadowRoot.querySelector('.bento-grid').appendChild(fragment)
-        }
-        else{
+
+            this.shadowRoot.querySelector('.bento-grid').appendChild(fragment);
+
+            const firstAlbumCard = this.shadowRoot.querySelector('album-card');
+            this.firstAlbumCard = firstAlbumCard
+            if (firstAlbumCard) {
+                firstAlbumCard.click();
+            }
+        } else {
             this.shadowRoot.innerHTML = /*html*/`
-            <link rel="stylesheet" href="../css/bentoFinded.css">
-            <div class="bento-finded">
-                <section class="bento-grid">
-                    <album-card id = 0></album-card>
-                    <album-card></album-card>
-                    <album-card></album-card>
-                    <album-card></album-card>
-                </section>
-            </div>
-        `
-        }     
-    
-       
+                <link rel="stylesheet" href="../css/bentoFinded.css">
+                <div class="bento-finded">
+                    <section class="bento-grid">
+                        <album-card id="0"></album-card>
+                        <album-card></album-card>
+                        <album-card></album-card>
+                        <album-card></album-card>
+                    </section>
+                </div>
+            `;
+        }
     }
 
     handleAlbumClick(id) {
         const event = new CustomEvent('albumClick', { detail: { albumId: id } });
         this.dispatchEvent(event);
     }
-   
+
+    onPageLoaded(){
+        this.firstAlbumCard?.click();
+    }
 }
