0b8e6ef2531c737aacabe959f30d84523b3258c2
DaroOps <romerosd089@hotmail.com>
2024-05-18 18:25:57 -0500

    fix: :bug: Fix css path of elements for deploy

--- js/components/musicPlayer/musicPlayer.js
+++ js/components/musicPlayer/musicPlayer.js
@@ -31,7 +31,7 @@ export class MusicPlayer extends HTMLElement {
         const trackAudio = this.getAttribute('audio-url') || "";
 
         this.shadowRoot.innerHTML = /*html*/`
-        <link rel="stylesheet" href="../css/musicPlayer.css">
+        <link rel="stylesheet" href="css/musicPlayer.css">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css" integrity="sha512-HK5fgLBL+xu6dm/Ii3z4xhlSUyZgTT9tuc/hSrtw6uzJOvgRr2a9jyxxT1ely+B+xFAmJKVSTbpM/CuL7qxO8w==" crossorigin="anonymous" />
         <div class="music-player">
             <div class="img-container skeleton">
